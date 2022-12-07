601cdf3082a0f46789dba1d402e0562c088b2657
annt <annt@rocketstudio.com.vn>
2022-12-08 00:54:58 +0700

    two columns radio button

--- app/src/main/java/com/example/weightconverter/MainActivity.java
+++ app/src/main/java/com/example/weightconverter/MainActivity.java
@@ -2,13 +2,68 @@ package com.example.weightconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+//region Toast Example
+//    Context context = getApplicationContext();
+//    CharSequence text = "Hello toast!";
+//    int duration = Toast.LENGTH_SHORT;
+//
+//    Toast toast = Toast.makeText(context, text, duration);
+//        toast.show();
+
+    //endregion
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView tv_From = (TextView) findViewById(R.id.tv_from_usd);
+        TextView tv_To = (TextView) findViewById(R.id.tv_to_VND);
+        Button btn_Convert = (Button) findViewById(R.id.btn_convert);
+        Button btn_cancel = (Button) findViewById(R.id.btn_clear);
+
+        btn_Convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        tv_From.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch1to2(view);
+            }
+        });
+
+        tv_To.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch1to2(view);
+            }
+        });
+
     }
+
+
+    public void switch1to2(View view) {
+        Intent switchActivityIntent = new Intent(this, MainActivity2.class);
+        startActivity(switchActivityIntent);
+
+    }
+
+
 }
--- app/src/main/java/com/example/weightconverter/MainActivity2.java
+++ app/src/main/java/com/example/weightconverter/MainActivity2.java
@@ -2,13 +2,122 @@ package com.example.weightconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button btn_cancel;
+    Button btn_convert;
+    RadioGroup rg1;
+    RadioGroup rg2;
+    RadioGroup rg3;
+    RadioGroup rg4;
+
+
+    private RadioGroup.OnCheckedChangeListener listener1 = new RadioGroup.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId != -1) {
+                rg2.setOnCheckedChangeListener(null); // remove the listener before clearing so we don't throw that stackoverflow exception(like Vladimir Volodin pointed out)
+                rg2.clearCheck(); // clear the second RadioGroup!
+                rg2.setOnCheckedChangeListener(listener2); //reset the listener
+
+            }
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener listener2 = new RadioGroup.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId != -1) {
+                rg1.setOnCheckedChangeListener(null);
+                rg1.clearCheck();
+                rg1.setOnCheckedChangeListener(listener1);
+
+            }
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener listener3 = new RadioGroup.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId != -1) {
+                rg4.setOnCheckedChangeListener(null); // remove the listener before clearing so we don't throw that stackoverflow exception(like Vladimir Volodin pointed out)
+                rg4.clearCheck(); // clear the second RadioGroup!
+                rg4.setOnCheckedChangeListener(listener4); //reset the listener
+
+            }
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener listener4 = new RadioGroup.OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId != -1) {
+                rg3.setOnCheckedChangeListener(null);
+                rg3.clearCheck();
+                rg3.setOnCheckedChangeListener(listener3);
+
+            }
+        }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btn_cancel = (Button) findViewById(R.id.btn_Cancel);
+        btn_convert = (Button) findViewById(R.id.btn_convert);
+
+        rg1 = (RadioGroup) findViewById(R.id.radio_group1);
+        rg2 = (RadioGroup) findViewById(R.id.radio_group_2);
+        rg3 = (RadioGroup) findViewById(R.id.radio_group3);
+        rg4 = (RadioGroup) findViewById(R.id.radio_group_4);
+
+        rg1.clearCheck();//this is so we can start fresh, with no selection on both RadioGroups
+        rg2.clearCheck();
+
+        rg3.clearCheck();
+        rg4.clearCheck();
+
+        rg1.setOnCheckedChangeListener(listener1);
+        rg2.setOnCheckedChangeListener(listener2);
+
+        rg3.setOnCheckedChangeListener(listener3);
+        rg4.setOnCheckedChangeListener(listener4);
+
+
+
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch2to1(view);
+            }
+        });
+
+
     }
+
+    public void switch2to1(View view) {
+        Intent switchActivityIntent = new Intent(this, MainActivity.class);
+        startActivity(switchActivityIntent);
+
+    }
+
+
+
 }
+
